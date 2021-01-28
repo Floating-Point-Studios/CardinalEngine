ff479d2cf62b2c089864ec6f14e1e4acf5c88bbc
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-01-28 03:51:52 -0600

    Update DeusSettings.lua

--- src/DeusSettings.lua
+++ src/DeusSettings.lua
@@ -8,9 +8,15 @@ return {
     -- Ignores modules parented to modules
     IgnoreSubmodules = true,
 
-    -- Adds Deus table to global shared table
+    -- Adds Deus table to global shared table accessible by shared.Deus()
     AttachToShared = false,
 
     -- Creates a module in ReplicatedStorage that can be used by any script
     PubliclyAccessibleLoader = false,
+
+    --[[
+        NOTE: If 'AttachToShared' is 'false' Deus will still set 'shared.Deus()' and will remove itself after it is finished loading itself.
+        If 'AttachToShared' and 'PubliclyAccessibleLoader' are false then 'shared.Deus()' will not be removed until it is run to allow
+        one-time access.
+    --]]
 }
