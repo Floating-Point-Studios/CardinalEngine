640df1c07a18fda21830a1c6ce093ca1e6d5f4c2
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-01-28 03:30:46 -0600

    Created CardinalEngine

---
+++ src/DeusSettings.lua
@@ -0,0 +1,16 @@
+return {
+    -- Ignores scripts that end in specific extensions such as "Example.config" by adding ".config"
+    IgnoredExtensions = {},
+
+    -- Doesn't laod modules until they are needed
+    LazyLoadModules = true,
+
+    -- Ignores modules parented to modules
+    IgnoreSubmodules = true,
+
+    -- Adds Deus table to global shared table
+    AttachToShared = false,
+
+    -- Creates a module in ReplicatedStorage that can be used by any script
+    PubliclyAccessibleLoader = false,
+}
