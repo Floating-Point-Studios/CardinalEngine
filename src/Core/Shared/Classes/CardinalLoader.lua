478653c8aaac8293d8fbcc738f1fd60afa882fb7
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-03-29 22:15:41 -0500

    Updated Loader security

--- src/Core/Shared/Classes/CardinalLoader.lua
+++ src/Core/Shared/Classes/CardinalLoader.lua
@@ -1,11 +1,16 @@
 local Deus
 
 local Output
+local TableUtils
 
 local ReservedNames = {
     "Cardinal",
     "Deus"
 }
+
+-- Store the modules here instead of Deus
+local Modules = {}
+local ModulesRefs = {}
 
 local CardinalLoader = {
     ClassName = "CardinalLoader",
@@ -27,19 +32,95 @@ function CardinalLoader:WrapModule(...)
     return Deus:WrapModule(...)
 end
 
+function CardinalLoader:WrapFunction(...)
+    return Deus:WrapFunction(...)
+end
+
 function CardinalLoader:IsRegistered(...)
     return Deus:IsRegistered(...)
 end
 
+function CardinalLoader:GetMainModule()
+    return Deus.GetMainModule(self)
+end
+
+function CardinalLoader:GetInitTick()
+    return Deus.GetInitTick(self)
+end
+
+function CardinalLoader:GetStartTick()
+    return Deus.GetStartTick(self)
+end
+
+function CardinalLoader:IsPluginFramework()
+    return Deus:IsPluginFramework()
+end
+
+--[[
+function CardinalLoader:GetModules()
+    local modulesData = {}
+
+    for path, module in pairs(Modules) do
+        local moduleData = {
+            InitTick = -1,
+            StartTick = -1,
+            IsInitialized = false,
+            IsStarted = false
+        }
+
+        if type(module) == "userdata" or type(module) == "function" then
+            if type(module) == "userdata" then
+                module = ModulesRefs[module]
+            end
+
+            moduleData.Module = module
+            moduleData.IsInitialized = true
+
+            print(getmetatable(module))
+        end
+
+        modulesData[path] = moduleData
+    end
+end
+]]
+
 function CardinalLoader:start()
-    Deus = {
-		Register = self.Register,
-		Load = self.Load,
-		WrapModule = self.WrapModule,
-		IsRegistered = self.IsRegistered
-	}
+    -- Proxy prevents modules from accessing Deus
+    Deus = getmetatable(getmetatable(self).__index).__index
 
     Output = self:Load("Deus.Output")
+    TableUtils = self:Load("Deus.TableUtils")
+
+    local DeusModules = Deus:GetModules()
+    for path, module in pairs(DeusModules) do
+        if type(module) == "table" then
+            local proxy = TableUtils.lock(module)
+            Modules[path] = proxy
+            ModulesRefs[proxy] = module
+        else
+            Modules[path] = module
+        end
+        DeusModules[path] = nil
+    end
+
+    -- We force these metamethods to run by transferring the Deus modules into our own modules variable which now allows us to detect every change being made by Deus
+    setmetatable(
+        DeusModules,
+        {
+            __index = function(_, path)
+                return Modules[path]
+            end,
+            __newindex = function(_, path, module)
+                if type(module) == "table" then
+                    local proxy = TableUtils.lock(module)
+                    Modules[path] = proxy
+                    ModulesRefs[proxy] = module
+                else
+                    Modules[path] = module
+                end
+            end
+        }
+    )
 
     self.PrivateProperties = {}
 
